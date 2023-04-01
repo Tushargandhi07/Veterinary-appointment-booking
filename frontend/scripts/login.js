63a18f84aa82aaf6cc0fa3b898c773cb53de5543
themanvendra00 <b.manvendrasingh2000@gmail.com>
2023-04-01 17:18:38 +0530

    Some css change

--- frontend/scripts/login.js
+++ frontend/scripts/login.js
@@ -5,9 +5,10 @@ let footerContainer = document.getElementById("footer");
 navbarContainer.innerHTML = navbar();
 footerContainer.innerHTML = footer();
 
+// setting username
 let userDetails = JSON.parse(localStorage.getItem("userDetails")) || null;
-let logged = JSON.parse(localStorage.getItem("loggedIn")) || null;
-if (logged) {
+
+if (userDetails) {
   document.getElementById("user").innerText = userDetails?.name;
   document.getElementById("loginbtn").innerText = "Logout";
 }
@@ -15,8 +16,8 @@ if (logged) {
 // redirect to account/login
 let login_icon = document.getElementById("loginbtn");
 login_icon.addEventListener("click", () => {
-  if (logged) {
-    localStorage.removeItem("loggedIn");
+  if (userDetails) {
+    localStorage.removeItem("userDetails");
     window.location.href = "login.html";
   } else {
     window.location.href = "login.html";
