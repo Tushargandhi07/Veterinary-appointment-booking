5e3ebd30979c075a4b0656a81fd88dbd81255287
themanvendra00 <b.manvendrasingh2000@gmail.com>
2023-04-01 17:30:10 +0530

    showing username && changing login state to logout

--- frontend/scripts/appointment_list.js
+++ frontend/scripts/appointment_list.js
@@ -12,6 +12,26 @@ logo.addEventListener("click", () => {
   window.location.href = "index.html";
 });
 
+// setting username
+let userDetails = JSON.parse(localStorage.getItem("userDetails")) || null;
+
+if (userDetails) {
+  document.getElementById("user").innerText = userDetails?.name;
+  document.getElementById("loginbtn").innerText = "Logout";
+}
+
+// redirect to account/login
+let login_icon = document.getElementById("loginbtn");
+login_icon.addEventListener("click", () => {
+  if (userDetails) {
+    localStorage.removeItem("userDetails");
+    window.location.href = "login.html";
+  } else {
+    window.location.href = "login.html";
+  }
+});
+
+
 
 // Display user profile
 function display(data) {
