0187905212a1a04b9d0794ade40eaea752889c53
Tushargandhi07 <tushargan7@gmail.com>
2023-04-02 18:56:35 +0530

    filter added on appointment list

--- frontend/scripts/appointment_list.js
+++ frontend/scripts/appointment_list.js
@@ -1,6 +1,7 @@
 import { navbar, footer } from "../components/navbar.js";
 let navbarContainer = document.getElementById("navbar");
 let footerContainer = document.getElementById("footer");
+let storeData =[];
 
 // Navbar
 navbarContainer.innerHTML = navbar();
@@ -15,10 +16,11 @@ logo.addEventListener("click", () => {
 // Display user profile
 function display(data) {
   const tbody = document.getElementById("tbody");
+  tbody.innerHTML = "";
   data.forEach((element) => {
     let row = document.createElement("tr");
     let col1 = document.createElement("td");
-    col1.innerText = element._id;
+    col1.innerText = element.doctor;
     let col2 = document.createElement("td");
     col2.innerText = element.date;
     let col3 = document.createElement("td");
@@ -32,14 +34,21 @@ function display(data) {
     } else {
       col4.style.color = "red";
     }
-    row.append(col1, col2, col3, col4);
+    let col5 = document.createElement("td");
+    if(element.roomId && element.status == "approved"){
+      col5.innerText = element.roomId;
+    }
+    else{
+      col5.innerText ="N/A"
+    }
+    row.append(col1, col2, col3, col5,col4);
     tbody.append(row);
   });
 }
 
+// Fetch Data from database
 let not_available = document.getElementById("not_available");
 async function getData() {
-  // let key ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySUQiOiI2NDI1NjcwYzRmZjJhY2RlYTRmZDQ0MWUiLCJpYXQiOjE2ODAxNzI4MzN9.WPSwGoSicD9yx25IxL1lkd1a8SnwzkicUTn_WvS6itA"
   let data = JSON.parse(localStorage.getItem("userDetails"));
   let userId = data._id;
   let email = data.email;
@@ -49,7 +58,6 @@ async function getData() {
         method: "GET",
         headers: {
           "Content-Type": "application/json",
-          // "Authorization":`${key}`,
           id: `${userId}`,
           email: `${email}`,
         },
@@ -59,6 +67,7 @@ async function getData() {
         not_available.style.display = "none";
       }
       console.log(data);
+      storeData = data;
       display(data);
     } catch (error) {
       console.log(error);
@@ -102,3 +111,16 @@ navRedirect.addEventListener("click", () => {
     });
   }
 });
+
+let filter = document.getElementById("filter");
+filter.addEventListener("change", () => {
+  if(filter.value==""){
+    display(storeData)
+  }
+  else{
+    let filterData= storeData.filter((ele)=>{
+      return ele.status == filter.value
+    });
+    display(filterData);
+  }
+})
